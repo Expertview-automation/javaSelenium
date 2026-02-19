493797e9691da6e3b2302b3041eecf59df95b4e7
vimleshBuaa <vimaleshyadav026@gmail.com>
2026-02-19 19:30:07 +0530

    19/2/26 create the add user automation scripting is in progress

--- src/main/java/org/example/pacticeAutomation/onlineTest.java
+++ src/main/java/org/example/pacticeAutomation/onlineTest.java
@@ -28,14 +28,26 @@ public class onlineTest {
        driver.findElement(By.xpath("//a/span[text()='Admin']")).click();
 
        driver.findElement(By.xpath("//label[text()='Username']/ancestor::div[contains(@class,'input-field-bottom-space')]//input")).sendKeys("yadav");
-       driver.findElement(By.xpath("   //label[text()='User Role']/ancestor::div[@class='oxd-input-group oxd-input-field-bottom-space']//i")).click();
+       driver.findElement(By.xpath("//label[text()='User Role']/ancestor::div[@class='oxd-input-group oxd-input-field-bottom-space']//i")).click();
 
-       driver.findElement(By.xpath("    //label[text()='User Role']//ancestor::div[@class='oxd-input-group oxd-input-field-bottom-space']//div[@class='oxd-select-text-input']")).click();//sendKeys("Admin");
+       driver.findElement(By.xpath("//label[text()='User Role']//ancestor::div[@class='oxd-input-group oxd-input-field-bottom-space']//div[@class='oxd-select-text-input']")).click();
        List<WebElement> listWeb = driver.findElements(By.xpath("//nav[@aria-label='Topbar Menu']//ul/li/span"));
+
 
        for (int i = 0; i < listWeb.size(); i++) {
           System.out.println(listWeb.get(i).getText());
           listWeb.get(i).click();
+
+         /* for (int j = 0; j < listWeb.size(); j++) {
+             System.out.println(listWeb.get(i).getText());
+             listWeb.get(j).click();
+          }*/
+
+          driver.findElement(By.xpath("//button[text()=' Add ']")).click();
+          driver.findElement(By.xpath("//label[text()='User Role']/ancestor::div[contains(@class,'oxd-input-group oxd-input-field-bottom-space')]//i")).click();
+          driver.findElement(By.xpath("//span[text()='ESS']")).click();
+          driver.findElement(By.xpath("//input[@placeholder='Type for hints...']")).sendKeys("Ranga  Akunuri");
+
        }
 
     }
